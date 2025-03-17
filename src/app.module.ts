23891f377941b723c1eec41a3ff233b7a91ba966
Noémie Goosen <noemie.goosen@gmail.com>
2025-03-17 15:23:20 +0100

    feat: app module type orm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mssql",
+      host: process.env.DB_HOST,
+      port: 1433,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      options: {
+        trustServerCertificate: true,
+      },
+      database: process.env.DB_NAME,
+      entities: [__dirname + "/**/*.entity{.ts,.js}"],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
