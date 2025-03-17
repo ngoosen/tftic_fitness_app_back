58ed304737413d4e418ae5a454927bb470772aab
Noémie Goosen <noemie.goosen@gmail.com>
2025-03-17 15:41:34 +0100

    feat: add config module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: "mssql",
       host: process.env.DB_HOST,
